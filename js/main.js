cb08e84bb52a0be6ac6de4ae09fd521ecd2a5ae2
Daniel McKemie <danielmckemie@gmail.com>
2018-07-24 01:40:07 -0400

    Version 1.2 (Logic insertions)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2]
-
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardTwo)
-
-if(cardsInPlay.length === 2) {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!');
-  } else {alert('Sorry, try again.')
-}
+  } else {alert('Sorry, try again.')};
+};
 }
 
-console.log('Up and running!');
-console.log(`User flipped ${cardTwo}.`);
+var flipCard = function(cardId) {
+	cardsInPlay.push(cardId);
+	console.log(`User flipped ${cards[cardId]}`);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
